b79488853cfae935219aec473f16d05e16102a6c
quasarlights <gastonestebancampagno@gmail.com>
2023-06-26 01:07:30 -0300

    Routes and Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
+import { Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Favs from "./Routes/Favs";
 
 
 function App() {
   return (
       <div className="App">
           <Navbar/>
+            <Routes>
+              <Route path="/" element= {<Home/>}/>
+              <Route path="/contact" element= {<Contact/>}/>
+              <Route path="/favs" element= {<Favs/>}/>
+            </Routes>
           <Footer/>
       </div>
   );
